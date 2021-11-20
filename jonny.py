92a38b4754715c1f2144ca94fec476956a9a2fe1
wake-code <87005812+wake-code@users.noreply.github.com>
2021-11-20 13:17:28 +0200

    add if, end, else ops

--- jonny.py
+++ jonny.py
@@ -1,3 +1,4 @@
+from re import I
 import sys
 import subprocess
 import shlex
@@ -24,6 +25,9 @@ OP_PLUS = iota()
 OP_MINUS = iota()
 OP_EQUAL = iota()
 OP_DUMP = iota()
+OP_IF = iota()
+OP_END = iota()
+OP_ELSE = iota()
 COUNT_OPS = iota()
 
 
@@ -47,38 +51,80 @@ def dump():
     return (OP_DUMP,)
 
 
-def simulate_program(prog):
-    stack = []
+def if_():
+    return (OP_IF,)
 
-    for op in prog:
-        assert COUNT_OPS == 5, "E: Exhaustive handling of ops in simulation"
+
+def end():
+    return (OP_END,)
+
+
+def else_():
+    return (OP_ELSE,)
+
+
+def simulate_program(program):
+    stack = []
+    ip = 0
+
+    while ip < len(program):
+        assert COUNT_OPS == 8, "E: Exhaustive handling of ops in simulation"
+
+        op = program[ip]
 
         if op[0] == OP_PUSH:
             stack.append(op[1])
+
+            ip += 1
         elif op[0] == OP_PLUS:
             a = stack.pop()
             b = stack.pop()
 
             stack.append(a + b)
+
+            ip += 1
         elif op[0] == OP_MINUS:
             a = stack.pop()
             b = stack.pop()
 
             stack.append(b - a)
+
+            ip += 1
         elif op[0] == OP_EQUAL:
             a = stack.pop()
             b = stack.pop()
 
             stack.append(int(a == b))
+
+            ip += 1
         elif op[0] == OP_DUMP:
             a = stack.pop()
 
             print(a)
+
+            ip += 1
+        elif op[0] == OP_IF:
+            a = stack.pop()
+
+            if a == 0:
+                assert (
+                    len(op) >= 2
+                ), "E: IF does not have a reference to the end of its block. Call crossreference_blocks() on the program before simulating to fix this."
+
+                ip = op[1]
+            else:
+                ip += 1
+        elif op[0] == OP_END:
+            ip += 1
+        elif op[0] == OP_ELSE:
+            assert (
+                len(op) >= 2
+            ), "E: ELSE does not have a reference to the end of its block. Call crossreference_blocks() on the program before simulating to fix this."
         else:
             assert False, f"E: Unreachable op '{op[0]}' in simulation"
 
 
-def compile_program(prog, out_file):
+def compile_program(program, out_file):
     with open(out_file, "w") as out:
         out.write("BITS 64\n")
         out.write("segment .text\n")
@@ -118,8 +164,10 @@ def compile_program(prog, out_file):
         out.write("global _start\n")
         out.write("_start:\n")
 
-        for op in prog:
-            assert COUNT_OPS == 5, "E: Exhaustive handling of ops in compilation"
+        for ip in range(len(program)):
+            op = program[ip]
+
+            assert COUNT_OPS == 8, "E: Exhaustive handling of ops in compilation"
 
             if op[0] == OP_PUSH:
                 out.write("    ;; -- push %d --\n" % op[1])
@@ -144,10 +192,32 @@ def compile_program(prog, out_file):
                 out.write("    pop rbx\n")
                 out.write("    cmp rax, rbx\n")
                 out.write("    cmove rcx, rdx\n")
+                out.write("    push rcx\n")
             elif op[0] == OP_DUMP:
                 out.write("    ;; -- dump --\n")
                 out.write("    pop rdi\n")
                 out.write("    call dump\n")
+            elif op[0] == OP_IF:
+                out.write("    ;; -- if --\n")
+                out.write("    pop rax\n")
+                out.write("    test rax, rax\n")
+
+                assert (
+                    len(op) >= 2
+                ), "E: if does not have a reference to the end of its block. Call crossreference_blocks() on the program before simulating to fix this."
+
+                out.write("    jz addr_%d\n" % op[1])
+            elif op[0] == OP_END:
+                out.write("addr_%d:\n" % ip)
+            elif op[0] == OP_ELSE:
+                out.write("    ;; -- else --\n")
+
+                assert (
+                    len(op) >= 2
+                ), "E: ELSE does not have a reference to the end of its block. Call crossreference_blocks() on the program before simulating to fix this."
+
+                out.write("    jmp addr_%d\n" % op[1])
+                out.write("addr_%d:\n" % (ip + 1))
             else:
                 assert False, "E: Unreachable op in compilation"
 
@@ -159,7 +229,7 @@ def compile_program(prog, out_file):
 def parse_token_as_op(token):
     (file_path, row, col, word) = token
 
-    assert COUNT_OPS == 5, "E: Exhaustive handling of ops in parsing"
+    assert COUNT_OPS == 8, "E: Exhaustive handling of ops in parsing"
 
     if word == "+":
         return plus()
@@ -169,12 +239,49 @@ def parse_token_as_op(token):
         return equal()
     elif word == ".":
         return dump()
+    elif word == "if":
+        return if_()
+    elif word == "end":
+        return end()
     else:
         try:
             return push(int(word))
         except ValueError as err:
             print("%s:%d:%d: %s" % (file_path, row, col, err))
             exit(1)
+
+
+def crossreference_blocks(program):
+    stack = []
+
+    for ip in range(len(program)):
+        op = program[ip]
+
+        assert (
+            COUNT_OPS == 8
+        ), "E: Exhaustive handling of ops in crossreferencing. No need to handle all ops in here. Only those that form blocks"
+
+        if op[0] == OP_IF:
+            stack.append(ip)
+        elif op[0] == OP_ELSE:
+            if_ip = stack.pop()
+
+            assert (
+                program[if_ip][0] == OP_IF
+            ), "W: END can only close IF blocks at the moment."
+
+            program[if_ip] = (OP_IF, ip + 1)
+
+            stack.append(ip)
+        elif op[0] == OP_END:
+            block_ip = stack.pop()
+
+            if program[block_ip][0] == OP_IF or program[block_ip][0] == OP_ELSE:
+                program[block_ip] = (program[block_ip][0], ip)
+            else:
+                assert False, "W: END can only close IF blocks at the moment."
+
+    return program
 
 
 def find_col(line, start, predicate):
@@ -185,7 +292,7 @@ def find_col(line, start, predicate):
 
 
 def lex_line(line):
-    col = find_col(line, 0, lambda x: x.isspace())
+    col = find_col(line, 0, lambda x: not x.isspace())
 
     while col < len(line):
         col_end = find_col(line, col, lambda x: x.isspace())
@@ -205,7 +312,9 @@ def lex_file(file_path):
 
 
 def load_program_from_file(file_path):
-    return [parse_token_as_op(token) for token in lex_file(file_path)]
+    return crossreference_blocks(
+        [parse_token_as_op(token) for token in lex_file(file_path)]
+    )
 
 
 def cmd_echoed(cmd):
